9135365766543dc7fe6354db5271a8e71d91cbc4
Kelvin J. Gómez <64044708+Kelvou@users.noreply.github.com>
2023-10-26 03:42:38 -0600

    Delete Proyecto2_9490-19-480/proyecto2 directory

--- Proyecto2_9490-19-480/proyecto2/src/AvisoError/AvisoErrorTypes.ts
+++
@@ -1,5 +0,0 @@
-export type AvisoErrorInterface = {
-    show: boolean;
-    handleClose: () => void;
-    mensaje: string;
-}
--- Proyecto2_9490-19-480/proyecto2/src/AvisoExitoso/AvisoExitosoInterface.ts
+++
@@ -1,6 +0,0 @@
-export interface AvisoExitosoProps {
-    show: boolean;
-    handleClose: () => void;
-    mensaje: string;
-    mensaje2: string;
-}
